5bfd0da4ebef05ff21392137377fcc964f0eab96
Elena-Paredes <elena.paredespacab@gmail.com>
2025-01-06 11:43:23 -0600

    Modificación de las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 /* Importación de rutas */
 import Home from "./pages/Home.jsx";
 import PDP from "./pages/PDP.jsx";
 import Checkout from "./pages/Checkout.jsx";
-import CartProvider from './context/Carrito'; // Proveedor del contexto del carrito
+import CartProvider from './context/Carrito'; 
 
 function App() {
   return (
-    <CartProvider> {/* Envolviendo toda la app con CartProvider */}
+    <CartProvider> 
       <Router>
         <Routes>
-          <Route path="home" element={<Home />} />
+          {/* Ruta raíz redirige a /home */}
+          <Route path="/" element={<Navigate to="/home" />} />
+          
+          {/* Rutas internas */}
+          <Route path="/home" element={<Home />} />
           <Route path="/producto/:id" element={<PDP />} />
-          <Route path="checkout" element={<Checkout />} />
-          {/*<Route path="signup" element={<SignUp />} />
-          <Route path="signin" element={<SignIn />} />*/}
+          <Route path="/checkout" element={<Checkout />} />
         </Routes>
       </Router>
     </CartProvider>
@@ -24,4 +26,5 @@ function App() {
 }
 
 export default App;
+
 
